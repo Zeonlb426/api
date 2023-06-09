96ddafed51482f6fc78e1cdbc67167b877ded987
DCH <zeonlb426@gmail.com>
2023-06-10 02:33:27 +0300

    Add migrate and seed

--- app.js
+++ app.js
@@ -1,130 +1,111 @@
-require("dotenv").config();
 const express = require("express");
-
-// const { Pool } = require('pg')
-
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: 5432,
-// })
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
 
 const app = express();
-
 app.use(express.json());
 
 // Register
-app.post("/register", async (req, res) => {
+// app.post("/register", async (req, res) => {
 
-    // Our register logic starts here
-    try {
-        // Get user input
-        const { first_name, last_name, email, password } = req.body;
+//     // Our register logic starts here
+//     try {
+//         // Get user input
+//         const { first_name, last_name, email, password } = req.body;
 
-        // Validate user input
-        if (!(email && password && first_name && last_name)) {
-        res.status(400).send("All input is required");
-        }
+//         // Validate user input
+//         if (!(email && password && first_name && last_name)) {
+//         res.status(400).send("All input is required");
+//         }
 
-        // check if user already exist
-        // Validate if user exist in our database
-        const oldUser = await User.findOne({ email });
+//         // check if user already exist
+//         // Validate if user exist in our database
+//         const oldUser = await User.findOne({ email });
 
-        if (oldUser) {
-        return res.status(409).send("User Already Exist. Please Login");
-        }
+//         if (oldUser) {
+//         return res.status(409).send("User Already Exist. Please Login");
+//         }
 
-        //Encrypt user password
-        encryptedPassword = await bcrypt.hash(password, 10);
+//         //Encrypt user password
+//         let encryptedPassword = await bcrypt.hash(password, 10);
 
-        // Create user in our database
-        const user = await User.create({
-        first_name,
-        last_name,
-        email: email.toLowerCase(), // sanitize: convert email to lowercase
-        password: encryptedPassword,
-        });
+//         // Create user in our database
+//         const user = await User.create({
+//         first_name,
+//         last_name,
+//         email: email.toLowerCase(), // sanitize: convert email to lowercase
+//         password: encryptedPassword,
+//         });
 
-        // Create token
-        const token = jwt.sign(
-            { user_id: user._id, email },
-            process.env.TOKEN_KEY,
-            {
-                expiresIn: "2h",
-            }
-        );
-        // save user token
-        user.token = token;
+//         // Create token
+//         const token = jwt.sign(
+//             { user_id: user._id, email },
+//             process.env.TOKEN_KEY,
+//             {
+//                 expiresIn: "2h",
+//             }
+//         );
+//         // save user token
+//         user.token = token;
 
-        // return new user
-        res.status(201).json(user);
-    } catch (err) {
-        console.log(err);
-    }
-    // Our register logic ends here
-});
+//         // return new user
+//         res.status(201).json(user);
+//     } catch (err) {
+//         console.log(err);
+//     }
+//     // Our register logic ends here
+// });
     
     // Login
-app.post("/login", async (req, res) => {
+// app.post("/login", async (req, res) => {
 
-        // Our login logic starts here
-        try {
-          // Get user input
-          const { email, password } = req.body;
+//         // Our login logic starts here
+//         try {
+//           // Get user input
+//           const { email, password } = req.body;
       
-          // Validate user input
-          if (!(email && password)) {
-            res.status(400).send("All input is required");
-          }
-          // Validate if user exist in our database
-          const user = await User.findOne({ email });
+//           // Validate user input
+//           if (!(email && password)) {
+//             res.status(400).send("All input is required");
+//           }
+//           // Validate if user exist in our database
+//           const user = await User.findOne({ email });
       
-          if (user && (await bcrypt.compare(password, user.password))) {
-            // Create token
-            const token = jwt.sign(
-              { user_id: user._id, email },
-              process.env.TOKEN_KEY,
-              {
-                expiresIn: "2h",
-              }
-            );
+//           if (user && (await bcrypt.compare(password, user.password))) {
+//             // Create token
+//             const token = jwt.sign(
+//               { user_id: user._id, email },
+//               process.env.TOKEN_KEY,
+//               {
+//                 expiresIn: "2h",
+//               }
+//             );
       
-            // save user token
-            user.token = token;
+//             // save user token
+//             user.token = token;
       
-            // user
-            res.status(200).json(user);
-          }
-          res.status(400).send("Invalid Credentials");
-        } catch (err) {
-          console.log(err);
-        }
-        // Our register logic ends here
-});
+//             // user
+//             res.status(200).json(user);
+//           }
+//           res.status(400).send("Invalid Credentials");
+//         } catch (err) {
+//           console.log(err);
+//         }
+//         // Our register logic ends here
+// });
 
 app.get("/", (req, res) => {
-    res.status(200).json({
-        message: 'Home page',
-    })
+    res.status(200).json(
+      {
+        message: process.env.API_PORT,
+      }
+    )
 });
 
-app.get("/login", (req, res) => {
-    res.status(200).json({
-        message: 'Hello World',
-    })
-});
+// const auth = require("./middleware/auth");
 
-const auth = require("./middleware/auth");
-
-app.post("/welcome", auth, (req, res) => {
-  res.status(200).send("Welcome 🙌 ");
-});
-
-// pool.query('SELECT NOW()', (err, res) => {
-//     console.log(err, res)
-//     pool.end()
-// })
+// app.post("/welcome", auth, (req, res) => {
+//   res.status(200).send("Welcome 🙌 ");
+// });
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 
-const port = process.env.API_PORT || 3000;
+const port = process.env.API_PORT || 5000;
 
 // server listening 
 server.listen(port, () => {
