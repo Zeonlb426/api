411891da239976fb0db81673e240ec13907688ea
DCH <zeonlb426@gmail.com>
2023-06-15 01:43:23 +0300

    Add swagger

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const router = require("./routes/router");
+// Документация API
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 
+const options = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "App Instagram API with Swagger",
+            version: "1.0.0",
+            description: "Это приложение основано на NodeJS Express и документировано при помощи Swagger",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+                name: "Instagram",
+                url: "https://instagram.lern.dev",
+                email: "dev@email.com",
+            },
+        },
+        servers: [
+            {
+            url: "https://instagram.lern.dev/api/v1/",
+            },
+        ],
+    },
+    apis: ["./routes/*.js", "./controllers/*.js"],
+};
+const specification = swaggerJsdoc(options);
+
 app.use(express.json());
 app.use('/v1/', router)
+
+app.use( '/v1/documentation', swaggerUi.serve, swaggerUi.setup(specification) );
 
 module.exports = app;
--- routes/router.js
+++ routes/router.js
@@ -9,6 +9,33 @@ const router = express.Router()
 router.get("/user", auth, userController.index);
 router.post("/logout", auth, userController.create);
 
+
+/**
+ * @swagger
+ * tags:
+ *   name: User Authentification
+ *   description: User Authentification managing API
+ * /register:
+ *   post:
+ *     summary: Create a new user account
+ *     tags: [User Authentification]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Register'
+ *     responses:
+ *       201:
+ *         description: The created user account.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Register'
+ *       500:
+ *         description: Some server error
+ *
+ */
 router.post("/register", validationRequest.request, userController.create);
 router.post("/login", authController.login);
 
